a1e10dd3bde23c7b2c5bc0d3e79fe5d078b1ca9f
benjaminmiharris <benjmiharris@gmail.com>
2024-07-15 10:40:19 +0300

    added project

---
+++ src/layouts/dashboard/config-navigation.jsx
@@ -0,0 +1,37 @@
+import SvgColor from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => (
+  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+);
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'clients',
+    path: '/clients',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'resources',
+    path: '/resources',
+    icon: icon('ic_cart'),
+  },
+  {
+    title: 'meetings',
+    path: '/meetings',
+    icon: icon('ic_blog'),
+  },
+  {
+    title: 'login',
+    path: '/login',
+    icon: icon('ic_lock'),
+  }
+];
+
+export default navConfig;
---
+++ src/sections/products/product-card.jsx
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+
+import Label from 'src/components/label';
+
+// ----------------------------------------------------------------------
+
+export default function ShopProductCard({ product }) {
+  const renderStatus = (
+    <Label
+      variant="filled"
+      color={(product.status === 'sale' && 'error') || 'info'}
+      sx={{
+        zIndex: 9,
+        top: 16,
+        right: 16,
+        position: 'absolute',
+        textTransform: 'uppercase',
+      }}
+    >
+      {product.status}
+    </Label>
+  );
+
+  const renderImg = (
+    <Box
+      component="img"
+      alt={product.name}
+      src={product.cover}
+      sx={{
+        top: 0,
+        width: 1,
+        height: 1,
+        objectFit: 'cover',
+        position: 'absolute',
+      }}
+    />
+  );
+
+
+
+  return (
+    <Card>
+      <Box sx={{ pt: '100%', position: 'relative' }}>
+        {product.status && renderStatus}
+
+        {renderImg}
+      </Box>
+
+      <Stack spacing={2} sx={{ p: 3 }}>
+        <Link color="inherit" underline="hover" variant="subtitle2" noWrap>
+          {product.name}
+        </Link>
+      </Stack>
+    </Card>
+  );
+}
+
+ShopProductCard.propTypes = {
+  product: PropTypes.object,
+};
